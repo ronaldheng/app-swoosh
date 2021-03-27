698913409c55dc6b85331ef49374f11347223fce
ronaldheng <34706750+ronaldheng@users.noreply.github.com>
2021-03-27 18:55:08 +0800

    lesson 09

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ronald Heng on 27/3/21.
+//  Copyright © 2021 Ronald Heng. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
